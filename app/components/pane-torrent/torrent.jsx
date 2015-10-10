dcc793ff203933d8f4a72c249c12bd736775c92d
ReklatsMasters <dmitrycvet@gmail.com>
2015-10-10 19:57:27 +0500

    hide empty fields in editor

--- app/components/pane-torrent/torrent.jsx
+++ app/components/pane-torrent/torrent.jsx
@@ -16,10 +16,10 @@ export default class Torrent extends Component {
       <div className='pane-torrent'>
         <Input name='infohash' value={torrent.infoHash} />
         <Input name='name' value={torrent.name} />
-        <Input name='comment' value={torrent.comment} />
+        {torrent.comment ? <Input name='comment' value={torrent.comment} /> : null}
         
         <Files files={torrent.files} />
-        <AnnounceList announce={torrent.announce} />
+        {torrent.announce.length ? <AnnounceList announce={torrent.announce} /> : null}
         <CreatedPrivate created={torrent.created} private={torrent.private} pieces={torrent.pieces.length} length={torrent.pieceLength} />
       </div>
     );
