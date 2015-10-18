852f2d8e70999d81a6125ada0ac817d797fccba1
ReklatsMasters <dmitrycvet@gmail.com>
2015-10-18 19:43:37 +0500

    + view for statistic for each tracker

--- app/components/pane-torrent/fields/announce-item/announce.jsx
+++ app/components/pane-torrent/fields/announce-item/announce.jsx
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
+import icon_spinner from '../../../../images/repeat.svg';
+
 import './announce.scss';
 
+function Spinner(props) {
+  return <span className='announce-spinner' dangerouslySetInnerHTML={{__html: icon_spinner}} />;
+}
+
 export default class AnnounceItem extends Component {
+  state = {
+    seeders: null,
+    leechers: null
+  }
+  
   static defaultProps = {
-    tracker: null
+    tracker: null,
+  }
+  
+  hasData() {
+    return this.state.seeders !== null && this.state.leechers !== null;
   }
   
   render() {
     return (
       <div className='announce-item'>
         <span className='announce-item_tracker'>{this.props.tracker}</span>
+        
+        <span className='announce-item_stat'>
+          {this.hasData() ? `${this.state.seeders} / ${this.state.leechers}` : <Spinner />}
+        </span>
       </div>
     );
   }
